624b66c7b50b35273ba64058d7e7492054b2bbb0
ezfx44 <nickich161@gmail.com>
2017-02-20 20:40:28 +0300

    Add files via upload

--- prog_1/prog_1/main.cpp
+++ prog_1/prog_1/main.cpp
@@ -1,23 +1,21 @@
 #include <iostream>
-#include <type_traits>
 
 int main()
 {
-	setlocale(LC_ALL, "Russian");
-	std::cout << "\t\tРазмеры фундаментальных типов данных в языке C++\nПорядковый арифметический тип:" << std::endl;
-	std::cout << "-bool \t\t- " << sizeof(bool) << " Б" << std::endl;
-	std::cout << "-char \t\t- " << sizeof(char) << " Б" << std::endl;
-	std::cout << "-char16_t \t- " << sizeof(char16_t) << " Б" << std::endl;
-	std::cout << "-char32_t \t- " << sizeof(char32_t) << " Б" << std::endl;
-	std::cout << "-wchar_t \t- " << sizeof(wchar_t) << " Б" << std::endl;
-	std::cout << "-short \t\t- " << sizeof(short) << " Б" << std::endl;
-	std::cout << "-int \t\t- " << sizeof(int) << " Б" << std::endl;
-	std::cout << "-long \t\t- " << sizeof(long) << " Б" << std::endl;
-	std::cout << "-long long \t- " << sizeof(long long) << " Б" << std::endl;
-	std::cout << "\nВещественный арифметический тип:" << std::endl;
-	std::cout << "-float \t\t- " << sizeof(float) << " Б" << std::endl;
-	std::cout << "-double \t- " << sizeof(double) << " Б" << std::endl;
-	std::cout << "-long double \t- " << sizeof(long double) << " Б" << std::endl;
+	std::cout << "\t\tThe fundamental data types in C++\n" << std::endl;
+	std::cout << "-bool \t\t- " << sizeof(bool) << " B" << std::endl;
+	std::cout << "-char \t\t- " << sizeof(char) << " B" << std::endl;
+	std::cout << "-char16_t \t- " << sizeof(char16_t) << " B" << std::endl;
+	std::cout << "-char32_t \t- " << sizeof(char32_t) << " B" << std::endl;
+	std::cout << "-wchar_t \t- " << sizeof(wchar_t) << " B" << std::endl;
+	std::cout << "-short \t\t- " << sizeof(short) << " B" << std::endl;
+	std::cout << "-int \t\t- " << sizeof(int) << " B" << std::endl;
+	std::cout << "-long \t\t- " << sizeof(long) << " B" << std::endl;
+	std::cout << "-long long \t- " << sizeof(long long) << " B" << std::endl;
+	std::cout << "-float \t\t- " << sizeof(float) << " B" << std::endl;
+	std::cout << "-double \t- " << sizeof(double) << " B" << std::endl;
+	std::cout << "-long double \t- " << sizeof(long double) << " B" << std::endl;
+	std::cout << "-nullptr_t \t- " << sizeof(nullptr_t) << " B" << std::endl;
 	std::cin.get();
 	return 0;
 }
